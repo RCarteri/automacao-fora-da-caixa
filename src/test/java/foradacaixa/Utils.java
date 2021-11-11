bfb5d32fc7f9bb443ba879fb0c072e72c8a8787f
Ricardo Carteri Uehlein <ricardo_carteri@hotmail.com>
2021-11-11 15:46:31 -0300

    5 - Coleta de logs Parte 2

---
+++ src/test/java/foradacaixa/Utils.java
@@ -0,0 +1,36 @@
+package foradacaixa;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+
+public class Utils {
+    public static void logPrint(String strLog) {
+        ExtentTest extentTest = TestRule.getExtentTest();
+        try {
+        	efetuarPrintTela(strLog);
+			extentTest.log(Status.INFO, strLog,	
+					MediaEntityBuilder.createScreenCaptureFromPath(System.getProperty("user.dir") + "/src/test/resources/report/" + strLog + ".png")
+                    .build());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    public static void efetuarPrintTela(String strLog) {
+		File scrFile = ((TakesScreenshot) TestRule.getDriver()).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(scrFile, new File("C:/Users/ruehlein/eclipse-workspace/foradacaixa/src/test/resources/report/" + strLog + ".png"));
+			System.out.print(System.getProperty("user.dir"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
